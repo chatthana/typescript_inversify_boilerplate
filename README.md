d3b863f65fcffee4e470cdcde03bd6aeb7dc4c26
Chatthana Janethanakarn <chattana.j@gmail.com>
2019-01-24 10:20:25 +0700

    README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# TypeScript / InversifyJS
+This is the boilerplate for TypeScript application with IoC container using InversifyJS
--- dist/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TYPES = {
-    Aircraft: Symbol.for("Aircraft"),
-    Wing: Symbol.for("Wing"),
-    Engine: Symbol.for("Engine")
-};
-exports.TYPES = TYPES;
